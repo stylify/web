e7ff8aa9a9e2cc0b3d09390535a5f27bc3bfd683
Vladimír <8machy@seznam.cz>
2022-10-04 15:24:58 +0200

    Stackblitz button update

--- components/StackBlitzLink.vue
+++ components/StackBlitzLink.vue
@@ -4,7 +4,7 @@
 		rel="noopener"
 		class="border-radius:8px line-height:1 font-size:16px display:inline-flex padding:8px_0px_8px_12px align-items:center color:#fff text-decoration:none font-weight:bold background:#3b98fc"
 	>
-		<span class="line-height:1">Edit on StackBlitz</span>
+		<span class="line-height:1">Try it on StackBlitz</span>
 		<img src="/images/brands/stackblitz-icon.jpg" alt="" width="34" height="34">
 	</a>
 </template>
