c20812d102381cb83d24a7f10e6cc5c31e539758
Vladimír <8machy@seznam.cz>
2022-12-09 14:08:15 +0100

    Fix

---
+++ components/StylifyFeatures.vue
@@ -0,0 +1,58 @@
+<!--
+stylify-components
+	'why-stylify__block': `
+		margin-left:12px margin-bottom:12px background:lighten($blue3,20) padding:12px
+		width:100% sm:width:calc(50%_-_12px) lg:width:calc(100%_*_1/3_-_12px) lg:margin-bottom:0 border-radius:$radius2
+	`,
+	'why-stylify__block-title': 'margin:0 font-size:22px display:flex flex-direction:row align-items:center',
+	'why-stylify__block-title-icon': 'font-weight:bold color:$blue1 width:24px height:24px display:inline-block margin-right:8px',
+	'why-stylify__list': 'font-size:14px margin-bottom:0 margin-top:4px padding-left:24px color:lighten($blue4,20)'
+/stylify-components
+-->
+<template>
+	<div id="why-stylify-css" class="display:flex margin-left:-12px flex-wrap:wrap [ul]{list-style:none;padding-left:0}">
+		<div class="why-stylify__block">
+			<h3 class="why-stylify__block-title">
+				<i class="icon icon-heart why-stylify__block-title-icon"></i>
+				<span>Write CSS faster</span>
+			</h3>
+			<ul class="why-stylify__list">
+				<li>💡 Use CSS-like selectors. Don't study framework</li>
+				<li>🔗 Define <nuxt-link to="/docs/stylify/compiler#variables" class="color:$blue1">Variables</nuxt-link>, <nuxt-link to="/docs/stylify/compiler#components" class="color:$blue1">Components</nuxt-link>, <nuxt-link to="/docs/stylify/compiler#customselectors" class="color:$blue1">Custom selectors</nuxt-link></li>
+				<li>🔁 Add <nuxt-link to="/docs/stylify/compiler#macros" class="color:$blue1">custom macros</nuxt-link> like <code>ml:2</code></li>
+				<li>💲 Variables can be injected into CSS as CSS variables</li>
+				<li>🌃 CSS <nuxt-link to="/docs/stylify/compiler#variables" class="color:$blue1">variables can differ for each screen</nuxt-link></li>
+				<li>🖌️ Simplify coding with <nuxt-link to="/docs/stylify/compiler#helpers" class="color:$blue1">helpers</nuxt-link> like <code>color:lighten(#000,10)</code></li>
+				<li>🖥️ Style any device with dynamic <nuxt-link to="/docs/stylify/compiler#screens" class="color:$blue1">screens</nuxt-link></li>
+			</ul>
+		</div>
+
+		<div class="why-stylify__block">
+			<h3 class="why-stylify__block-title">
+				<i class="icon icon-layers why-stylify__block-title-icon"></i>
+				<span>Get extremely optimized CSS</span>
+			</h3>
+			<ul class="why-stylify__list">
+				<li>📦 Split bundles for page/layout/component</li>
+				<li>🧰 Minified selectors<code>.color:blue</code> to short <code>.a</code></li>
+				<li>✨ No purge needed. CSS is generated on demand</li>
+				<li>🔗 Almost no no duplicated <code>property:value</code></li>
+				<li>✋ Mark areas that <nuxt-link to="/docs/stylify/compiler#ignoredareas" class="color:$blue1">should not be processed</nuxt-link></li>
+				<li>🪝 Use hooks to extend functionality</li>
+			</ul>
+		</div>
+
+		<div class="why-stylify__block">
+			<h3 class="why-stylify__block-title">
+				<i class="icon icon-layers why-stylify__block-title-icon"></i>
+				<span>Copy&paste integration</span>
+			</h3>
+			<ul class="why-stylify__list">
+				<li>🚀 Works with any framework like <nuxt-link to="/docs/integrations/nuxtjs" class="color:$blue1">Nuxt.js</nuxt-link>, <nuxt-link to="/docs/integrations/nextjs" class="color:$blue1">Next.js</nuxt-link>, <nuxt-link to="/docs/integrations/astro" class="color:$blue1">Astro</nuxt-link>. <nuxt-link to="/docs/integrations/solidjs" class="color:$blue1">SolidJS</nuxt-link>, <nuxt-link to="/docs/integrations/qwik" class="color:$blue1">Qwik</nuxt-link> <nuxt-link to="/docs/integrations/symfony" class="color:$blue1">Symfony</nuxt-link>, <nuxt-link to="/docs/integrations/nette" class="color:$blue1">Nette</nuxt-link>, <nuxt-link to="/docs/integrations/laravel" class="color:$blue1">Laravel</nuxt-link></li>
+				<li>⚒️ Integrable into bundlers like <nuxt-link to="/docs/integrations/webpack" class="color:$blue1">Webpack</nuxt-link>, <nuxt-link to="/docs/integrations/rollupjs" class="color:$blue1">Rollup</nuxt-link>, <nuxt-link to="/docs/integrations/vitejs" class="color:$blue1">Vite.js</nuxt-link></li>
+				<li>⛓️ Generated CSS can be used within SCSS, Less, Stylus</li>
+				<li>🎨 CSS variables can be exported into external file</li>
+			</ul>
+		</div>
+	</div>
+</template>
--- components/WhyStylify.vue
+++ components/WhyStylify.vue
@@ -1,58 +1,27 @@
-<!--
-stylify-components
-	'why-stylify__block': `
-		margin-left:12px margin-bottom:12px background:lighten($blue3,20) padding:12px
-		width:100% sm:width:calc(50%_-_12px) lg:width:calc(100%_*_1/3_-_12px) lg:margin-bottom:0 border-radius:$radius2
-	`,
-	'why-stylify__block-title': 'margin:0 font-size:22px display:flex flex-direction:row align-items:center',
-	'why-stylify__block-title-icon': 'font-weight:bold color:$blue1 width:24px height:24px display:inline-block margin-right:8px',
-	'why-stylify__list': 'font-size:14px margin-bottom:0 margin-top:4px padding-left:24px color:lighten($blue4,20)'
-/stylify-components
--->
 <template>
-	<div id="why-stylify-css" class="display:flex margin-left:-12px flex-wrap:wrap [ul]{list-style:none;padding-left:0}">
-		<div class="why-stylify__block">
-			<h3 class="why-stylify__block-title">
-				<i class="icon icon-heart why-stylify__block-title-icon"></i>
-				<span>Write CSS faster</span>
-			</h3>
-			<ul class="why-stylify__list">
-				<li>💡 Use CSS-like selectors. Don't study framework</li>
-				<li>🔗 Define <nuxt-link to="/docs/stylify/compiler#variables" class="color:$blue1">Variables</nuxt-link>, <nuxt-link to="/docs/stylify/compiler#components" class="color:$blue1">Components</nuxt-link>, <nuxt-link to="/docs/stylify/compiler#customselectors" class="color:$blue1">Custom selectors</nuxt-link></li>
-				<li>🔁 Add <nuxt-link to="/docs/stylify/compiler#macros" class="color:$blue1">custom macros</nuxt-link> like <code>ml:2</code></li>
-				<li>💲 Variables can be injected into CSS as CSS variables</li>
-				<li>🌃 CSS <nuxt-link to="/docs/stylify/compiler#variables" class="color:$blue1">variables can differ for each screen</nuxt-link></li>
-				<li>🖌️ Simplify coding with <nuxt-link to="/docs/stylify/compiler#helpers" class="color:$blue1">helpers</nuxt-link> like <code>color:lighten(#000,10)</code></li>
-				<li>🖥️ Style any device with dynamic <nuxt-link to="/docs/stylify/compiler#screens" class="color:$blue1">screens</nuxt-link></li>
-			</ul>
-		</div>
-
-		<div class="why-stylify__block">
-			<h3 class="why-stylify__block-title">
-				<i class="icon icon-layers why-stylify__block-title-icon"></i>
-				<span>Get extremely optimized CSS</span>
-			</h3>
-			<ul class="why-stylify__list">
-				<li>📦 Split bundles for page/layout/component</li>
-				<li>🧰 Minified selectors<code>.color:blue</code> to short <code>.a</code></li>
-				<li>✨ No purge needed. CSS is generated on demand</li>
-				<li>🔗 Almost no no duplicated <code>property:value</code></li>
-				<li>✋ Mark areas that <nuxt-link to="/docs/stylify/compiler#ignoredareas" class="color:$blue1">should not be processed</nuxt-link></li>
-				<li>🪝 Use hooks to extend functionality</li>
-			</ul>
-		</div>
-
-		<div class="why-stylify__block">
-			<h3 class="why-stylify__block-title">
-				<i class="icon icon-layers why-stylify__block-title-icon"></i>
-				<span>Copy&paste integration</span>
-			</h3>
-			<ul class="why-stylify__list">
-				<li>🚀 Works with any framework like <nuxt-link to="/docs/integrations/nuxtjs" class="color:$blue1">Nuxt.js</nuxt-link>, <nuxt-link to="/docs/integrations/nextjs" class="color:$blue1">Next.js</nuxt-link>, <nuxt-link to="/docs/integrations/astro" class="color:$blue1">Astro</nuxt-link>. <nuxt-link to="/docs/integrations/solidjs" class="color:$blue1">SolidJS</nuxt-link>, <nuxt-link to="/docs/integrations/qwik" class="color:$blue1">Qwik</nuxt-link> <nuxt-link to="/docs/integrations/symfony" class="color:$blue1">Symfony</nuxt-link>, <nuxt-link to="/docs/integrations/nette" class="color:$blue1">Nette</nuxt-link>, <nuxt-link to="/docs/integrations/laravel" class="color:$blue1">Laravel</nuxt-link></li>
-				<li>⚒️ Integrable into bundlers like <nuxt-link to="/docs/integrations/webpack" class="color:$blue1">Webpack</nuxt-link>, <nuxt-link to="/docs/integrations/rollupjs" class="color:$blue1">Rollup</nuxt-link>, <nuxt-link to="/docs/integrations/vitejs" class="color:$blue1">Vite.js</nuxt-link></li>
-				<li>⛓️ Generated CSS can be used within SCSS, Less, Stylus</li>
-				<li>🎨 CSS variables can be exported into external file</li>
-			</ul>
+	<div :style="`text-align:${textAlign}`">
+		<h2 class="margin-bottom:8px">Why Stylify CSS?</h2>
+		<p>
+			To style websites we need CSS. Writing CSS in optimized way is not easy. It requires skills.
+			<br> Stylify may look like inline styles. Why not use them? Because they cannot be efficiently optimized.
+		</p>
+		<div class="hp__info-boxes text-align:left margin-top:8px font-size:14px display:grid grid-template-columns:repeat(auto-fit,minmax(300px,1fr)) gap:12px">
+			<div>
+				<strong class="color:#fff">Problems when writing CSS</strong>
+				<br>Splitting CSS, clearing unused classes, duplicates in CSS property:value, duplicated media queries, unnecessary selectors chaining, counterintuitive nesting in preprocessors, unnecessary high CSS specificity, maintaining selectors that are modified on multiple places, creating self-explanatory selectors for cases where you need to style only one element and change only it's margin, combining files so only used CSS is imported, CSS files organization and naming, minifying selectors in CSS+HTML, etc.
+			</div>
+			<div>
+				<strong class="color:#fff">How Stylify CSS solves this?</strong>
+				<br>Stylify uses CSS-like syntax to stay as close to CSS syntax as possible (so <strong>devs don't have to study random shortcuts or create them</strong>, even though it makes selectors longer) and in the same time it focuses on optimization problems (so it is easier for devs to maintain the CSS size) providing <strong>simple bundles splitting, automated bundles mapping, generating used CSS, components, variables, selectors shrinking</strong>, etc.
+			</div>
 		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	props: {
+		textAlign: 'left'
+	}
+}
+</script>
