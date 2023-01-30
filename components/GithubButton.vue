75430d2f02cc98688358d53e14d62c47166891fb
Vladimír <8machy@seznam.cz>
2023-01-30 11:10:40 +0100

    Update stars

--- components/GithubButton.vue
+++ components/GithubButton.vue
@@ -23,7 +23,7 @@ stylify-keyframes
 <script>
 export default {
 	data: () => ({
-		stars: process.client && typeof window?.stylifyGithubStars === 'number' ? window.stylifyGithubStars : '307'
+		stars: process.client && typeof window?.stylifyGithubStars === 'number' ? window.stylifyGithubStars : '310'
 	}),
 	props: {
 		type: {
