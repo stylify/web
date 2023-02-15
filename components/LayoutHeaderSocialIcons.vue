eb9d93c926009c28275e1d033994a5ebe38dad3e
Vladimír <8machy@seznam.cz>
2023-02-15 22:48:07 +0100

    Fix

--- components/LayoutHeaderSocialIcons.vue
+++ components/LayoutHeaderSocialIcons.vue
@@ -6,7 +6,7 @@ stylify-components
 <template>
 	<nav class="display:flex align-items:center">
 		<div class="display:flex height:40px width:84px margin-right:8px"><GithubButton /></div>
-		<div class="display:flex height:40px width:92px"><GithubButton type="sponsor" /></div>
+		<div class="display:none minw410px:display:flex height:40px width:92px"><GithubButton type="sponsor" /></div>
 		<a :href="project.config.discordUrl" target="_blank" rel="noopener" title="Discord" aria-label="Discord" class="display:inline-flex"><img src="/images/brands/discord-icon.svg" height="25" width="31" class="layout-header-icon-link" alt="" loading="lazy" /></a>
 		<a :href="project.config.twitterUrl" target="_blank" rel="noopener" title="Twitter" aria-label="Twitter" class="display:inline-flex"><img src="/images/brands/twitter-icon.svg" height="25" width="25" class="layout-header-icon-link" alt="" loading="lazy" fetchpriority="low"/></a>
 		<a :href="project.config.facebookUrl" target="_blank" rel="noopener" title="Facebook" aria-label="Youtube" class="display:inline-flex"><img src="/images/brands/facebook-icon.svg" height="24" width="32" class="layout-header-icon-link" alt="" loading="lazy" /></a>
